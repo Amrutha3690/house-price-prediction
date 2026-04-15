33100d465faec007507eeb84f07883dfe8e6893e
Amrutha3690 <auppugan@gitam.in>
2026-04-15 23:10:08 +0530

    House Price Prediction 2

--- app/app.py
+++ app/app.py
@@ -3,8 +3,7 @@ import pickle
 
 app = Flask(__name__)
 
-model = pickle.load(open('../model/model.pkl', 'rb'))
-
+model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
